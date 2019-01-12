908f2bdd85f6480dc7787404a546c29b4e5ea4e5
Sean Esteva <github@cryptology.codes>
2019-01-12 06:41:26 -0800

    creating components

---
+++ app/components/Main.js
@@ -0,0 +1,12 @@
+// React required here
+var React = require('react');
+// Including the Link component from React Router to navigate within our app without full page reloads
+
+var Link = require('react-router').Link;
+
+// Now for the Main component
+var Main = React.createClass({render: function(){
+	return (
+	
+)
+}});
